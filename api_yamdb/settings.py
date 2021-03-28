095f82b19c564b96348ec66501a9c910177136f7
Roman <safonov.roman.86@yandex.ru>
2021-03-28 23:23:44 +1000

    First try abort

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -16,7 +16,7 @@ from decouple import config
 import os  # без этой строки тесты не запустятся
 
 
-print(os.path)  # а без этой строки тесты считают, что зря мы os импортировали
+# print(os.path)  # а без этой строки тесты считают, что зря мы os импортировали
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
